843b47297ed112ec16676fdbfcc1743374b228f9
elias_eshetu <eliaseshetu28@gmail.com>
2023-11-02 20:19:21 +0300

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,11 +7,12 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
+	if (tree == NULL)
+		return (0);
 
-    size_t left_nodes = binary_tree_nodes(tree->left);
-    size_t right_nodes = binary_tree_nodes(tree->right);
+	size_t left_nodes = binary_tree_nodes(tree->left);
+	size_t right_nodes = binary_tree_nodes(tree->right);
 
-    return left_nodes + right_nodes + (tree->left != NULL || tree->right != NULL);
+	return (left_nodes + right_nodes + (tree->left != NULL
+				|| tree->right != NULL));
 }
