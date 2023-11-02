2e192d0413f9b895d83b58acecbe7f0b2c6a3ce6
elias_eshetu <eliaseshetu28@gmail.com>
2023-11-02 21:26:38 +0300

    18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,16 +7,11 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-		return NULL;
-
-	binary_tree_t *grandparent = node->parent->parent;
-
-	if (grandparent->left == node->parent)
-		return grandparent->right;
-
-	if (grandparent->right == node->parent)
-		return grandparent->left;
-
-	return NULL;
+	if (node == NULL ||
+	    node->parent == NULL ||
+	    node->parent->parent == NULL)
+		return (NULL);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
